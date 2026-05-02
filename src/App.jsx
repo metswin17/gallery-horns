dd928c75f569daa029d83ec4928dd1777a5639b1
Luis Lopez <metfanforever@gmail.com>
2026-05-01 23:48:42 -0700

    mod pack-lock pack.json & srcApp

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
+
+import data from './data.json';
+
+import
+'bootstrap/dist/css/bootstrap.mim.css'
 import Header from './components/Header'
 // import Gallery from './components/Gallery'
 import Footer from './components/Footer'
+import './data.json'
 import './App.css'
 
 function App() {
