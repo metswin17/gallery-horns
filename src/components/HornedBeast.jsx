fb295a626b43258660435a626995486aa5db013b
Luis Lopez <metfanforever@gmail.com>
2026-05-13 23:17:23 -0700

    mod Horned,rename-Selected

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 
 function HornedBeast({ beast, handleOpenModal }) {
+
+  const [votes, setVotes] = useState(0);
+
+const handleVotes = () => {
+  setVotes(prevVotes => prevVotes + 1);
+};
+
   return (
     <div>
       <h2>{beast.title}</h2>
+      <p>❤️ Favorites: {votes}</p>
 
       <img
-        src={beast.image_url}
-        alt={beast.title}
-        title={beast.title}
-        onClick={() => handleOpenModal(beast)}
-      />
+  src={beast.image_url}
+  alt={beast.title}
+  title={beast.title}
+  onClick={() => {
+    handleVotes();
+    handleOpenModal(beast);
+  }}
+  style={{ width: '300px' }}
+/>
 
       <p>{beast.description}</p>
     </div>
