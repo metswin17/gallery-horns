e89748e8087eedf31d2f1636bfafc2e121cb0ebc
Luis Lopez <metfanforever@gmail.com>
2026-05-19 01:00:21 -0700

    App.jsx mod

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,20 @@ import Gallery from './components/Gallery';
 import Footer from './components/Footer';
 import SelectedBeast from './components/SelectedBeast';
 
+import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   const [galleryData] = useState(data);
+
+  const [selectedHorns, setSelectedHorns] = useState('All');
+  
+  const handleHornChange = (event) => {
+    setSelectedHorns(event.target.value);
+  };
+
+
 
   const [selectedBeast, setSelectedBeast] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
@@ -29,13 +38,33 @@ function App() {
   const filteredBeasts = galleryData.filter((beast) => {
     const regex = new RegExp(searchTerm, 'i');
   
-    return regex.test(beast.title);
+    const matchesSearch = regex.test(beast.title);
+  
+    const matchesHorns =
+      selectedHorns === 'All' ||
+      beast.horns === Number(selectedHorns);
+  
+    return matchesSearch && matchesHorns;
   });
 
   return (
     <>
       <Header />
       
+      <Form>
+  <Form.Group>
+    <Form.Label>Filter by Number of Horns</Form.Label>
+
+    <Form.Select onChange={handleHornChange}>
+      <option value="All">All</option>
+      <option value="1">1 Horn</option>
+      <option value="2">2 Horns</option>
+      <option value="3">3 Horns</option>
+      <option value="100">100 Horns</option>
+    </Form.Select>
+  </Form.Group>
+</Form>
+
     <input
   className="search-bar"
   type="text"
